86ee00389798ed3397160ccb756f973012ebebdd
Chathurga <chathurga@gmail.com>
2013-11-19 14:38:14 +0000

    Updated main.c

--- src/main.c
+++ src/main.c
@@ -7,28 +7,27 @@
 int main() {
   struct timespec *start = timer_start();
   
-  CPLEX *cplex = cplex_start();
-  Problem *problem = init_problem("./data/ire100.tsp", cplex);
+  Problem problem = tsp_init("./data/ire100.tsp");
+  CPLEX cplex = cplex_start();
+  PassOutput output = cplex_init(problem, cplex);
   
   int presolve = timer_end(start);
   printf("Presolve time: %dms\n\n", presolve);
   printf("No.   Distance   Subtours   Time\n");
   
-  PassOutput *output = NULL;
-  while (1) {
+  while (output.n != 1) {
     output = cplex_pass(problem, output, cplex);
     
     printf("%3d   %-8.2f   %-8d   %dms\n",
-           output->i, output->distance, output->n, output->cplex_time);
-    
-    if (output->n == 1) break;
+           output.i, output.distance, output.n, output.cplex_time);
     
     cplex_constrain(output, cplex);
   }
   
   int total = timer_end(start);
+  free(start);
   
-  printf("\nSolved! Best Route: %.2f\n", output->distance);
+  printf("\nSolved! Best Route: %.2f\n", output.distance);
   printf("Solve Time: %dms\n\n", total - presolve);
   printf("Total Time: %dms\n\n", total);
   
